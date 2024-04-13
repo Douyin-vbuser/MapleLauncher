9f526602e49ddcfab7a67f915213a6864eaa623b
Douyin-vbuser <douyin_vbuser@outlook.com>
2024-04-13 20:48:21 +0800

    initial mdk

--- preload.js
+++ preload.js
@@ -1,11 +1,3 @@
-/**
- * The preload script runs before `index.html` is loaded
- * in the renderer. It has access to web APIs as well as
- * Electron's renderer process modules and some polyfilled
- * Node.js functions.
- *
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
